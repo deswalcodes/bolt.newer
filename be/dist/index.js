730ef000eb343e3e094c00db90264abe9ea1853a
deswalcodes <21cs2014@rgipt.ac.in>
2025-05-10 19:56:43 +0530

    added template router

--- be/dist/index.js
+++ be/dist/index.js
@@ -13,20 +13,61 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
+const express_1 = __importDefault(require("express"));
+const node_1 = require("./defaults/node");
+const react_1 = require("./defaults/react");
 const sdk_1 = __importDefault(require("@anthropic-ai/sdk"));
+const prompts_1 = require("./prompts");
 const anthropic = new sdk_1.default();
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const msg = yield anthropic.messages.create({
-            model: "claude-3-5-sonnet-20241022",
-            max_tokens: 1000,
-            temperature: 0,
-            messages: [{
-                    role: 'user',
-                    content: "what is capital of india"
-                }]
-        });
-        console.log(msg);
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.post('/template', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const prompt = req.body.prompt;
+    const response = yield anthropic.messages.create({
+        model: "claude-3-5-sonnet-20241022",
+        max_tokens: 200,
+        temperature: 0,
+        system: "Return either node or react based on what do you think this project should be.Only return a single word either 'node' or 'react'.Do not return anything extra",
+        messages: [{
+                role: 'user',
+                content: prompt
+            }]
     });
-}
-main();
+    const answer = response.content[0].text;
+    if (answer === "react") {
+        res.json({
+            prompts: [prompts_1.BASE_PROMPT, react_1.basePrompt]
+        });
+    }
+    if (answer === "node") {
+        res.json({
+            prompts: [node_1.basePrompt]
+        });
+    }
+    res.status(403).json({
+        message: "ypu cant access"
+    });
+    return;
+}));
+// async function main(){
+//    anthropic.messages.stream({
+//         model : "claude-3-5-sonnet-20241022",
+//         max_tokens : 1000,
+//         temperature : 0,
+//         system : getSystemPrompt(),
+//         messages : [{
+//             role : 'user' ,
+//             content : ""
+//         },{
+//             role : 'user',
+//             content : "Here is an artifact that contains all files of project visible to you.\nConsider the contents of ALL files in the project.\n\n{{BASE_PROMPT}} \n\nHere is a list of files that exist on the file system but are not shown to you:\n\n - .gitignore\n -package-lock.json\n - .bolt/prompt"
+//         },{
+//             role : 'user',
+//             content : "what is capital of india"
+//         }]
+//     }).on('text',(text)=>{
+//         console.log(text);
+//     });
+// }
+// main();
+app.listen(3000);
--- be/src/index.ts
+++ be/src/index.ts
@@ -15,7 +15,7 @@ app.post('/template',async (req,res)=>{
     const prompt = req.body.prompt;
     const response = await anthropic.messages.create({
         model : "claude-3-5-sonnet-20241022",
-        max_tokens : 1000,
+        max_tokens : 200,
         temperature : 0,
         system : "Return either node or react based on what do you think this project should be.Only return a single word either 'node' or 'react'.Do not return anything extra",
         messages : [{
@@ -24,22 +24,23 @@ app.post('/template',async (req,res)=>{
         }]
     });
     const answer = (response.content[0] as TextBlock).text;
-    if(answer != "react" && answer != "node"){
-        res.status(403).json({
-            message : "You cant access this"
-        })
-        return
-    }
+   
     if(answer === "react"){
         res.json({
-            prompts : [BASE_PROMPT,reactBasePrompt]
+            prompts : [BASE_PROMPT,`Here is an artifact that contains all files of project visible to you.\nConsider the contents of ALL files in the project.\n\n${{reactBasePrompt}} \n\nHere is a list of files that exist on the file system but are not shown to you:\n\n - .gitignore\n -package-lock.json\n`],
+            uiPrompts : [reactBasePrompt]
         })
     }
     if(answer === "node"){
         res.json({
-            prompts : [nodeBasePrompt]
+            prompts : [`Here is an artifact that contains all files of project visible to you.\nConsider the contents of ALL files in the project.\n\n${{nodeBasePrompt}} \n\nHere is a list of files that exist on the file system but are not shown to you:\n\n - .gitignore\n -package-lock.json\n`],
+            uiPrompts : [nodeBasePrompt]
         })
     }
+    res.status(403).json({
+        message : "you cant access"
+    })
+    return;
 
 
 
@@ -75,3 +76,4 @@ async function main(){
 }
 
 main();
+app.listen(3000);
